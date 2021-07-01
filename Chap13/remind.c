2828d269a46e9fd69ca1ac447dadbc69bb1dd1c8
NorthSeaPark <33457633+NorthSeaPark@users.noreply.github.com>
2021-07-01 00:46:51 -0400

    Add files via upload

---
+++ Chap13/remind.c
@@ -0,0 +1,13 @@
+/*Prints a one-month reminder list*/
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_REMIND 50
+#define MSG_LEN 60
+
+int read_line(char str[],int n);
+
+int main()
+{
+	
+}
